f7e39993ba5ad4617e997234bc49dbc8021ee30f
b166er <delagneauadrien@yahoo.fr>
2025-08-23 09:01:07 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 
 
-import HomePage from "@/components/home-page";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start pt-24">
-      <HomePage />
+   
     </main>
   );
 }
